ae13588d0f1b12a3db71358cc214857c60841f10
FatWhite <3338504@qq.com>
2018-03-20 18:28:04 +0800

    DENGLU

--- CJWL/app/src/main/java/cjwl/cjb/org/view/activity/LoginActivity.java
+++ CJWL/app/src/main/java/cjwl/cjb/org/view/activity/LoginActivity.java
@@ -1,9 +1,39 @@
 package cjwl.cjb.org.view.activity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+
+import cjwl.cjb.org.R;
+
 /**
  * 登录
  * Created by jimbai on 2018/3/18.
  */
 
-public class LoginActivity {
+public class LoginActivity extends Activity{
+
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    private void initView(){
+        btnLogin=findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent();
+                intent.setClass(LoginActivity.this,MapActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
 }
